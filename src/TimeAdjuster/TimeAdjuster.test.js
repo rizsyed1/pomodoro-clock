340ceedfc6f5182294563bea27afd38424964f30
RizwanSyed357 <rizwansyed357@protonmail.com>
2019-04-25 14:52:20 +0100

    Completed functionality test of arrowDown FontAwesome icon onClick callback

--- src/TimeAdjuster/TimeAdjuster.test.js
+++ src/TimeAdjuster/TimeAdjuster.test.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import TimeAdjuster from './TimeAdjuster.js'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
-import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 import App from '../App.js'
 
 describe(('TimeAdjuster'), () => {
     it('should render a downwards-pointing FontAwesome arrow icon whose onClick callback increments time by one minute when clicked', () => {
         const AppWrapper = mount(<App />)
-
-        const mockdownArrowClick = () => {
+  
+        const mockdownArrowClick = jest.fn(() => {
             AppWrapper.setState({
-              workTime: AppWrapper.state.workTime - 1
+              workTime: AppWrapper.state().workTime - 1
             })
-        }
-        
-        const wrapper = mount(<TimeAdjuster time={25} downArrowClick={mockdownArrowClick} timeAdjusterName='Session Length' />);
+        })
+
+
+        const wrapper = mount(<TimeAdjuster time={AppWrapper.state().workTime} downArrowClick={mockdownArrowClick} timeAdjusterName='Session Length' />);
         wrapper.find('.arrowDown').simulate('click');
-        expect(wrapper.contains(<span className='time'>{24}</span>)).toBe(true);
+        const wrapper2 =  mount(<TimeAdjuster time={AppWrapper.state().workTime} downArrowClick={mockdownArrowClick} timeAdjusterName='Session Length' />);
+        expect(wrapper2.contains(<span className='time'>{24}</span>)).toEqual(true)
     });
 
+    // This a partly-written commented out test that I am going to start working on once the above one works properly. 
     // it('should render a upwards-pointing FontAwesome arrow icon whose onClick callback increments time by one minute when clicked', () => {
     //     const wrapper = 
     // })
