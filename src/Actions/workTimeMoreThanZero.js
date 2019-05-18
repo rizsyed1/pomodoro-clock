792e5493c2fda77d0437bef5296edc9ec52b2e31
RizwanSyed357 <rizwansyed357@protonmail.com>
2019-05-18 14:26:00 +0100

    added styling and removed conditionally rendering of Timer component

--- src/Actions/workTimeMoreThanZero.js
+++
@@ -1,9 +0,0 @@
- let workTimeMoreThanZero = () => { // checks state.workTime won't go below zero & state.countDown is false
-  if (this.state.workTimeMinutes >= 1 && !this.state.countDown ){
-    return true;
-  } else {
-    return false;
-  }
-}
-
-export default workTimeMoreThanZero
--- src/Timer/Timer.js
+++ src/Timer/Timer.js
@@ -1,40 +1,32 @@
 import React from 'react'; 
 import './Timer.css';
 
-let timeDisplay = ( seconds) => {
+let timeDisplay = ( seconds ) => {
         let returnedMinutes = Math.floor(seconds / 60)
         let returnedSeconds = Math.floor(seconds % 60)
-        if (returnedSeconds.toString().length < 2 && returnedMinutes.toString().length < 2) {
-            return `0${returnedMinutes}:0${returnedSeconds}`
 
-        } else if (returnedMinutes.toString().length < 2) {
-            return `0${returnedMinutes}:${returnedSeconds}`
-
-        } else if (returnedSeconds.toString().length < 2) {
-            return `${returnedMinutes}:0${returnedSeconds}`
-
-        } else {
-            return `${returnedMinutes}:${returnedSeconds}`
+        if ( returnedMinutes.toString().length < 2) {
+            returnedMinutes = `0${returnedMinutes}`
         }
+
+        if (returnedSeconds.toString().length < 2){
+            returnedSeconds = `0${returnedSeconds}` 
+        }
+
+        return `${returnedMinutes}:${returnedSeconds}`
 }
 
+    
+
+
 function Timer({timeLeft, workTimer, timeAdjusterWorkTime}) {
-    if(workTimer === ''){
         return (
             <div className='timerContainer'>
                 <span className='titleSession'>Session</span>
-                <span className='timeDisplay'>{timeDisplay(timeAdjusterWorkTime)}</span>
-            </div>
-        )  
-    } else {
-        return (
-            <div className='timerContainer'>
-                <span className='titleSession'>Session</span>
-                <span className='timeDisplay'>{timeDisplay(timeLeft)}</span>
+                <span className='timeDisplay'>{workTimer === ''? timeDisplay(timeAdjusterWorkTime): timeDisplay(timeLeft)}</span>
             </div>
         )  
     }
-        
-}
+
 
 export default Timer; 
